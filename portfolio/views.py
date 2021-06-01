1a025fe63ddf971081459b39c03a34e48bb8f726
Ryota Higashi <ryo19870130@outlook.jp>
2021-06-01 23:50:33 +0900

    学習日数表示

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,15 +1,20 @@
 from django.shortcuts import render
 from .models import Profile, Skill, Work
+import datetime
 
 def index(request):
   profile = Profile.objects.all().last()
   skills = Skill.objects.all().order_by('genre')
   works = Work.objects.all().order_by('-published')[:3]
+  start_day = datetime.date(2020, 11, 21)
+  days = (datetime.date.today() - start_day).days + 1
 
   context = {
     'profile': profile,
     'skills': skills,
     'works': works,
+    'start_day': start_day,
+    'days': days,
   }
   return render(request, 'index.html', context)
 
